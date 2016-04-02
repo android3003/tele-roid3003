d439f00b8821844b978190d59d66ca282854313d
android3003 <jonoonsorat@gmail.com>
2016-04-02 15:59:55 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = { 102255473},--Sudo users
+    sudo_users = {97653520},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
